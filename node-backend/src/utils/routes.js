6564333af3b9b9b68f6fd5aa48d1f2b866c314bd
brian tran <btran@hamilton.edu>
2025-06-14 21:25:41 -0700

    no delete

--- node-backend/src/utils/routes.js
+++ node-backend/src/utils/routes.js
@@ -28,7 +28,6 @@ export default function routes(app) {
   app.post("/api/user", createUser);
   app.get("/api/user/:id", getUserById);
   app.get("/api/users", getAllUsers);
-  app.delete("/api/user/:id", deleteUserById);
   app.put("/api/user/:id", updateUserById);
 
   app.get("/", (req, res) => res.status(200).send("foo OK"));
